5aa0e7cbefb92f65266c2835d54295a4d6fc1a15
Jonathan Latties <johnlatties@gmail.com>
2021-05-08 18:31:19 -0300

    chore: prettier setup

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <div>
+      <main>
+        <h1>
+          Starting new app with <a href="https://nextjs.org">Next.js!</a>
+        </h1>
+      </main>
+    </div>
+  )
+}
